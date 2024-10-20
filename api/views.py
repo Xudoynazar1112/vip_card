9754cb05727bbe705cdf68c5d567186b78c14687
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-10-20 17:00:41 +0500

    added post request

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from product.models import Product
 from .serializers import ProductSerializer
 
 
-class ProductListAPIView(ListAPIView):
+class ProductListAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
